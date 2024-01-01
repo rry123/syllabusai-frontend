b3c9afae0ddbbd217006fa6e4eb3aa024bbd15cc
bprathamesh20 <bprathamesh2003@gmail.com>
2024-01-01 16:54:08 +0530

    added support for html content

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import NotesIcon from '../components/NotesIcon'
 import TestIcon from '../components/TestIcon'
 import LoadingBar from '../components/LoadingBar'
+import DOMPurify from 'dompurify';
+import '../content.css'
 
 
 export default function Playground() {
@@ -24,12 +26,13 @@ export default function Playground() {
         body: raw,
         redirect: 'follow'
         };
-        fetch("https://syllabus-ai.onrender.com/notes", requestOptions)
+        fetch("https://syllabus-ai.onrender.com/test", requestOptions)
         .then(response => response.json()) // Parse the response as JSON
         .then(data => {
             const content = data.data; // Extract content from the "data" field
-            console.log(content); // Optional: Log the content if needed
-            setContent(content); // Set the content value to the variable
+            console.log(content); 
+            const sanitizedHtml = DOMPurify.sanitize(content);
+            setContent(sanitizedHtml); // Set the content value to the variable
             setisLoading(false)
         })
         .catch(error => console.log('error', error));
@@ -39,11 +42,11 @@ export default function Playground() {
     }
 
 
-
-
   return (
-    <div className='flex flex-col items-center  lg:w-4/12   p-1'>
+    <div className='flex flex-col items-center  lg:w-4/12   p-3'>
     <div className='flex flex-col w-full gap-4'>
+        
+
        <h2 className="text-2xl font-bold leading-7 text-gray-300 sm:truncate sm:text-3xl sm:tracking-tight">
          Generate 
         </h2>
@@ -67,11 +70,12 @@ export default function Playground() {
 
         </div>
 
-        <div>
-            <p>
-                {isLoading ? <LoadingBar/> : content}
+        <div className='flex items-center justify-center w-full'>
+          
                 
-            </p>
+            {isLoading ? <LoadingBar/> : <div dangerouslySetInnerHTML={{ __html: content }}  className='flex flex-col gap-2 content'/>}
+                
+ 
         </div>
 
     </div>
